reocat/qiikzx-gsraviole-apatch

commit 6b47a64b8a0539f297d07c7a59a80c485c00fbfb

Makefile: Consolidate build targets
The recipes for the build targets clean, modules, modules_install and
headers_install are basically identical. Let's consolidate them.

Signed-off-by: Daniel Mentz <danielmentz@google.com>
Change-Id: Idad42663bb497468bf31a2c9c3ec43fcd849f097

Makefile
@@ -18,17 +18,7 @@ ccflags-y := -I$(KERNEL_SRC)/../google-modules/video/gchips
 
 EXTRA_CFLAGS	+= -I$(KERNEL_SRC)/../google-modules/video/gchips/include
 
-modules modules_install:
-	$(MAKE) -C $(KERNEL_SRC) M=$(M) \
-	$(KBUILD_OPTIONS) \
-	EXTRA_CFLAGS="$(EXTRA_CFLAGS)" \
-	$(@)
-
-headers_install:
-	$(MAKE) -C $(KERNEL_SRC) M=$(M) \
-	$(@)
-
-clean:
+clean modules modules_install headers_install:
 	$(MAKE) -C $(KERNEL_SRC) M=$(M) \
 	$(KBUILD_OPTIONS) \
 	EXTRA_CFLAGS="$(EXTRA_CFLAGS)" \

commit 1fca46b2148771e350ae197ac50ffc894f9d90bc

Makefile: Make "modules" the default target
Commit 6b47a64 ("Makefile: Consolidate build targets") introduced a
regression where it changed the default make target from "modules" to
"clean". As a result, bigocean.ko was not built at all! Change the
default target back to "modules".

Fixes: 6b47a64 ("Makefile: Consolidate build targets")
Signed-off-by: Daniel Mentz <danielmentz@google.com>
Change-Id: I7ca51f81b573eb773ada30dff6c2123d223bd0e9

Makefile
@@ -18,7 +18,7 @@ ccflags-y := -I$(KERNEL_SRC)/../google-modules/video/gchips
 
 EXTRA_CFLAGS	+= -I$(KERNEL_SRC)/../google-modules/video/gchips/include
 
-clean modules modules_install headers_install:
+modules modules_install headers_install clean:
 	$(MAKE) -C $(KERNEL_SRC) M=$(M) \
 	$(KBUILD_OPTIONS) \
 	EXTRA_CFLAGS="$(EXTRA_CFLAGS)" \