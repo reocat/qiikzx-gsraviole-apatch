8186e1c05ed1d51c5f61f517b17e6348bebed2a9
Taylor Nelms <tknelms@google.com>
2022-12-07 17:20:50 +0000

    samsung: support sysfs counter node for decon
    Bug: 240346564
    Test: Build for C10 or O6 device,
      cat /sys/class/drm/card0/device/decon0/decon_counters
      or same with decon2 device
    Merged-In: Ief68f59dc5207367c731b75bc7cf550dd7629280
    Change-Id: Ief68f59dc5207367c731b75bc7cf550dd7629280
    Signed-off-by: Taylor Nelms <tknelms@google.com>

--- samsung/exynos_drm_decon.c
+++ samsung/exynos_drm_decon.c
@@ -1547,6 +1547,7 @@ static int decon_bind(struct device *dev, struct device *master, void *data)
 	struct exynos_drm_private *priv = drm_to_exynos_dev(drm_dev);
 	struct drm_plane *default_plane;
 	int i;
+	char symlink_name_buffer[7];
 
 	decon->drm_dev = drm_dev;
 
@@ -1591,6 +1592,11 @@ static int decon_bind(struct device *dev, struct device *master, void *data)
 		decon->bts.ops->init(decon);
 	}
 
+	/* Create symlink to decon device */
+	snprintf(symlink_name_buffer, 7, "decon%d", decon->id);
+	sysfs_create_link(&decon->drm_dev->dev->kobj, &decon->dev->kobj,
+			  (const char *) symlink_name_buffer);
+
 	device_create_file(dev, &dev_attr_early_wakeup);
 	decon_debug(decon, "%s -\n", __func__);
 	return 0;
@@ -1599,9 +1605,15 @@ static int decon_bind(struct device *dev, struct device *master, void *data)
 static void decon_unbind(struct device *dev, struct device *master,
 			void *data)
 {
+	char symlink_name_buffer[7];
 	struct decon_device *decon = dev_get_drvdata(dev);
-
 	decon_debug(decon, "%s +\n", __func__);
+
+	/* Remove symlink to decon device */
+	snprintf(symlink_name_buffer, 7, "decon%d", decon->id);
+	sysfs_remove_link(&decon->drm_dev->dev->kobj,
+			  (const char *) symlink_name_buffer);
+
 	device_remove_file(dev, &dev_attr_early_wakeup);
 	if (IS_ENABLED(CONFIG_EXYNOS_BTS))
 		decon->bts.ops->deinit(decon);
