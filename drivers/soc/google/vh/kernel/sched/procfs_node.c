17f0fafcdf9a5c81888595ab525a8879b31ed959
Aarqw12 <lcockx@protonmail.com>
2023-04-11 23:13:05 -0700

    LLVM 18 : fix all warning

--- drivers/soc/google/vh/kernel/sched/procfs_node.c
+++ drivers/soc/google/vh/kernel/sched/procfs_node.c
@@ -1934,7 +1934,7 @@ static ssize_t pmu_poll_time_store(struct file *filp,
 
 PROC_OPS_RW(pmu_poll_time);
 
-static  pmu_poll_enable_show(struct seq_file *m, void *v)
+static int pmu_poll_enable_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%s\n", pmu_poll_enabled ? "true" : "false");
 	return 0;
@@ -1975,7 +1975,7 @@ PROC_OPS_RW(pmu_poll_enable);
 
 extern unsigned int sched_lib_cpu_freq_cached_val;
 
-static sched_lib_cpu_freq_cached_show(struct seq_file *m, void *v)
+static int sched_lib_cpu_freq_cached_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%u\n", sched_lib_cpu_freq_cached_val);
 	return 0;
@@ -2007,7 +2007,7 @@ static ssize_t sched_lib_cpu_freq_cached_store(struct file *filp,
 PROC_OPS_RW(sched_lib_cpu_freq_cached);
 
 extern unsigned int sched_lib_freq_cpumask;
-static sched_lib_freq_cpumask_show(struct seq_file *m, void *v)
+static int sched_lib_freq_cpumask_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%d\n", sched_lib_freq_cpumask);
 	return 0;
@@ -2038,7 +2038,7 @@ static ssize_t sched_lib_freq_cpumask_store(struct file *filp,
 PROC_OPS_RW(sched_lib_freq_cpumask);
 
 extern unsigned int sched_lib_affinity_val;
-static sched_lib_affinity_show(struct seq_file *m, void *v)
+static int sched_lib_affinity_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%d\n", sched_lib_affinity_val);
 	return 0;
@@ -2071,13 +2071,13 @@ PROC_OPS_RW(sched_lib_affinity);
 extern ssize_t sched_lib_name_store(struct file *filp,
 				const char __user *ubuffer, size_t count,
 				loff_t *ppos);
-extern sched_lib_name_show(struct seq_file *m, void *v);
+extern int sched_lib_name_show(struct seq_file *m, void *v);
 
 
 PROC_OPS_RW(sched_lib_name);
 
 /* uclamp filters controls */
-static uclamp_min_filter_enable_show(struct seq_file *m, void *v)
+static int uclamp_min_filter_enable_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%d\n", static_branch_likely(&uclamp_min_filter_enable) ? 1 : 0);
 	return 0;
@@ -2109,7 +2109,7 @@ static ssize_t uclamp_min_filter_enable_store(struct file *filp,
 }
 PROC_OPS_RW(uclamp_min_filter_enable);
 
-static uclamp_min_filter_us_show(struct seq_file *m, void *v)
+static int uclamp_min_filter_us_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%d\n", sysctl_sched_uclamp_min_filter_us);
 	return 0;
@@ -2165,7 +2165,7 @@ static ssize_t uclamp_min_filter_rt_store(struct file *filp,
 }
 PROC_OPS_RW(uclamp_min_filter_rt);
 
-static uclamp_max_filter_enable_show(struct seq_file *m, void *v)
+static int uclamp_max_filter_enable_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%d\n", static_branch_likely(&uclamp_max_filter_enable) ? 1 : 0);
 	return 0;
@@ -2197,7 +2197,7 @@ static ssize_t uclamp_max_filter_enable_store(struct file *filp,
 }
 PROC_OPS_RW(uclamp_max_filter_enable);
 
-static uclamp_max_filter_divider_show(struct seq_file *m, void *v)
+static int uclamp_max_filter_divider_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%d\n", sysctl_sched_uclamp_max_filter_divider);
 	return 0;
