a4576945ecccdb3b0604fd2e70631a4332e2984d
Jack Diver <diverj@google.com>
2024-03-13 16:33:34 +0000

    [DO NOT MERGE ANYWHERE] Revert "mgm: sysfs node to manually retain SLC partition"
    Revert submission 2753879-gpu-slcv2-gs201
    
    Reason for revert: Prebuild did not land before cutoff
    
    Reverted changes: /q/submissionid:2753879-gpu-slcv2-gs201
    
    Bug: 329447972
    Change-Id: I8216424d3713d063b0f24f33a65162541cfda408

--- mali_pixel/pixel_slc.h
+++ mali_pixel/pixel_slc.h
@@ -54,9 +54,6 @@ struct slc_partition {
 
 	/** @signal: Partition enable/disable signal from SLC governor */
 	u64 signal;
-
-	/** @pinned: Is the partition pinned to the enabled state */
-	bool pinned;
 };
 
 /**
@@ -90,8 +87,6 @@ u64 slc_wipe_pbha(u64 pte);
 void slc_inc_refcount(struct slc_data *data);
 
 void slc_dec_refcount(struct slc_data *data);
-
-void slc_pin(struct slc_data *data, bool pin);
 
 void slc_update_signal(struct slc_data *data, u64 signal);
 
