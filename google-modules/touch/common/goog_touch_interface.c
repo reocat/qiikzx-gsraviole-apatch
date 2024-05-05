kerneltoast/android_kernel_google_zuma

goog_touch: Elide goog_input_process() when unneeded
goog_input_process() takes a mutex lock and does some other bookkeeping,
even when it isn't needed. Elide goog_input_process() when touch heatmap,
offload, and the debug kfifo are all disabled.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

google-modules/touch/common/goog_touch_interface.c
@@ -3417,7 +3417,11 @@ void goog_input_release_all_fingers(struct goog_touch_interface *gti)
 
 	goog_input_unlock(gti);
 
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_OFFLOAD) || \
+    IS_ENABLED(CONFIG_TOUCHSCREEN_HEATMAP) || \
+    defined(GTI_DEBUG_INPUT_KFIFO_LEN)
 	goog_input_process(gti, true);
+#endif
 }
 
 void goog_register_tbn(struct goog_touch_interface *gti)
@@ -4316,7 +4320,11 @@ static irqreturn_t gti_irq_thread_fn(int irq, void *data)
 	else
 		ret = IRQ_HANDLED;
 
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_OFFLOAD) || \
+    IS_ENABLED(CONFIG_TOUCHSCREEN_HEATMAP) || \
+    defined(GTI_DEBUG_INPUT_KFIFO_LEN)
 	goog_input_process(gti, false);
+#endif
 
 	mutex_unlock(&gti->input_heatmap_lock);
 

reocat/qiikzx-gsraviole-apatch

google-modules/touch/common/goog_touch_interface.c
@@ -2686,7 +2686,11 @@ void goog_input_release_all_fingers(struct goog_touch_interface *gti)
 
 	goog_input_unlock(gti);
 
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_OFFLOAD) || \
+    IS_ENABLED(CONFIG_TOUCHSCREEN_HEATMAP) || \
+    defined(GTI_DEBUG_KFIFO_LEN)
 	goog_input_process(gti, true);
+#endif
 }
 
 void goog_register_tbn(struct goog_touch_interface *gti)
@@ -3550,7 +3554,11 @@ static irqreturn_t gti_irq_thread_fn(int irq, void *data)
 	else
 		ret = IRQ_HANDLED;
 
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_OFFLOAD) || \
+    IS_ENABLED(CONFIG_TOUCHSCREEN_HEATMAP) || \
+    defined(GTI_DEBUG_KFIFO_LEN)
 	goog_input_process(gti, false);
+#endif
 
 	mutex_unlock(&gti->input_heatmap_lock);
 